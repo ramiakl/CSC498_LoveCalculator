b2a5e0eaffc3e7138d1d82e73a79be5b1112295c
Rami <ramiakl33@gmail.com>
2022-02-14 16:21:40 +0200

    Adding a calculate button and testing the output of the spinner

--- app/src/main/java/com/lau/geeklovecalculator/MainActivity.java
+++ app/src/main/java/com/lau/geeklovecalculator/MainActivity.java
@@ -3,13 +3,17 @@ package com.lau.geeklovecalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     Spinner spinner;
     ArrayAdapter<String> adapter;
+    String language;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +27,11 @@ public class MainActivity extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
 
         spinner.setAdapter(adapter);
+    }
+
+    public void calculate(View v){
+
+        language = spinner.getSelectedItem().toString();
+        Toast.makeText(this, language, Toast.LENGTH_SHORT).show();
     }
 }
