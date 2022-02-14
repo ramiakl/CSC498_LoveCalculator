f5ca4096e961625aab1b2ad10e626abe4208409e
Rami <ramiakl33@gmail.com>
2022-02-14 13:44:56 +0200

    Setting up the spinner and adding the progamming languages

--- app/src/main/java/com/lau/geeklovecalculator/MainActivity.java
+++ app/src/main/java/com/lau/geeklovecalculator/MainActivity.java
@@ -3,12 +3,25 @@ package com.lau.geeklovecalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+    Spinner spinner;
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spinner = (Spinner) findViewById(R.id.spinner1);
+
+        adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.languages));
+
+        adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+
+        spinner.setAdapter(adapter);
     }
 }
