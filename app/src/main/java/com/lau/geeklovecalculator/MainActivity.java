cf80865ac41654c80440ad3c93b6aac07ced2512
Rami <ramiakl33@gmail.com>
2022-02-15 15:27:22 +0200

    Adding the logo of the appropriate language with animation

--- app/src/main/java/com/lau/geeklovecalculator/MainActivity.java
+++ app/src/main/java/com/lau/geeklovecalculator/MainActivity.java
@@ -7,10 +7,12 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Locale;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
     private Random rand = new Random();
     private int percentage_love;
     private TextView result;
+    ImageView img1;
+    ImageView logo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +44,12 @@ public class MainActivity extends AppCompatActivity {
 
         result = (TextView) findViewById(R.id.result);
         result.setText("");
+
+        img1 = (ImageView) findViewById(R.id.img1);
+
+        logo = (ImageView) findViewById(R.id.img2);
+
+        logo.setTranslationY(-1500);
     }
 
     public void calculate(View v){
@@ -53,6 +63,31 @@ public class MainActivity extends AppCompatActivity {
 
         text.setVisibility(View.GONE);
         spinner.setVisibility(View.GONE);
+
+        Log.i("Logo:", language);
+
+        switch(language.toLowerCase(Locale.ROOT)){
+
+            case "java":
+                logo.setImageResource(R.drawable.java);
+                break;
+            case "python":
+                logo.setImageResource(R.drawable.python);
+                break;
+            case "c":
+                logo.setImageResource(R.drawable.c);
+                break;
+            case "r":
+                logo.setImageResource(R.drawable.r);
+                break;
+            case "sql":
+                logo.setImageResource(R.drawable.sql);
+                break;
+            case "php":
+                logo.setImageResource(R.drawable.php);
+                break;
+        }
+        logo.animate().translationYBy(1500).rotation(3600).setDuration(600);
 
 
     }
